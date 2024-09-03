7b86bf54b6cc14a20e50d3a45c6502e9e78e3285
Sangram-tech <sangrammahakud1@gmail.com>
2024-09-03 15:40:54 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+
+// wap to calcualte area of a cricle:
+int main(){
+float radius;
+printf(" enter radius:");
+scanf("%f", &radius);
+printf("area:%f",3.14 * radius * radius);
+
+return 0;
+
+}
